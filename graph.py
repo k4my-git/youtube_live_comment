9ab748d557d60d0759874b9249615794c1232c55
k4my <65527834+k4my-git@users.noreply.github.com>
2021-09-14 06:16:22 +0900

    update

--- graph.py
+++ graph.py
@@ -2,11 +2,11 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
-fname = input("insert ID >>") + ".csv"
+fname = input("insert ID >>")
 word = input("insert Word >>")
 interval = int(input("insert interval >>"))
 
-with open(fname) as f:
+with open("data/"+fname+".csv") as f:
     reader = csv.reader(f)
     l = [row for row in reader]
 
@@ -19,7 +19,7 @@ def seconds(times):
         second = int(s) + (int(m) * 60) + (int(h) * 3600)
     return second
 
-
+    
 last = l[-1][0]
 cut_time = seconds(last) // interval
 
@@ -28,6 +28,7 @@ y = []
 
 start = 0
 count = 0
+
 for i in range(cut_time):
     for lists in l:
         if seconds(lists[0]) >= interval*i and seconds(lists[0]) < interval*(i+1):
@@ -36,11 +37,27 @@ for i in range(cut_time):
     y.append(count)
     count = 0
 
+dic = {}
+for i in range(len(x)):
+    key = int(x[i]) - interval
+    dic[key] = y[i]
+dic2 = dict(sorted(dic.items(), key=lambda x:x[1], reverse=True))
+
+print("Ranking by"+word)
+rank = 1
+for i in range(3):
+    url = 'https://youtu.be/'+fname+'?t='+str(list(dic2.keys())[i])
+    print(str(rank)+"st "+url)
+    rank += 1
+
 fig = plt.figure()
 plt.title("Highlight", fontsize=24)
 plt.xlabel("times(s)", fontsize=24)
 plt.ylabel("count", fontsize=24)
 plt.grid(True)
-plt.plot(x, y, marker="o")
+plt.plot(x, y, marker="o", color = "blue")
 fig.set_size_inches(15, 5)
+fig.tight_layout()
+#plt.savefig(fname+'.png')
 plt.show()
+
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from urllib.parse import urlparse, parse_qs
 import sys
 import csv
 import time
+import os
 
 vid = input("insert ID >>")
 target_url = "https://youtu.be/" + vid
@@ -15,36 +16,30 @@ comment_data = []
 session = requests_html.HTMLSession()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'}
 
-# まず動画ページにrequestsを実行しhtmlソースを手に入れてlive_chat_replayの先頭のurlを入手
 resp = session.get(target_url)
 resp.html.render(sleep=3)
 for iframe in resp.html.find("iframe"):
     if("live_chat_replay" in iframe.attrs["src"]):
         next_url= "".join(["https://www.youtube.com", iframe.attrs["src"]])
-
+print("Getting comment...")
+count=0
 try:
     while(1):
         try:
-            time.sleep(2)
+            time.sleep(1)
             html = session.get(next_url, headers=headers)
             soup = BeautifulSoup(html.text,"lxml")
 
-            # 次に飛ぶurlのデータがある部分をfind_allで探してsplitで整形
             for scrp in soup.find_all("script"):
                 if "window[\"ytInitialData\"]" in scrp.next:
                     dict_str = scrp.next.split(" = ", 1)[1]
 
-            # 辞書形式と認識すると簡単にデータを取得できるが, 末尾に邪魔なのがあるので消しておく（「空白2つ + \n + ;」を消す）
             dict_str = dict_str.rstrip("  \n;")
-            # 辞書形式に変換
             dics = json.loads(dict_str)
 
-            # "https://www.youtube.com/live_chat_replay?continuation=" + continue_url が次のlive_chat_replayのurl
             continue_url = dics["continuationContents"]["liveChatContinuation"]["continuations"][0]["liveChatReplayContinuationData"]["continuation"]
             next_url = "https://www.youtube.com/live_chat_replay?continuation=" + continue_url
-            # dics["continuationContents"]["liveChatContinuation"]["actions"]がコメントデータのリスト。
             for samp in dics["continuationContents"]["liveChatContinuation"]["actions"][1:]:
-                #comment_data.append(str(samp)+"\n")
                 if 'addChatItemAction' not in samp["replayChatItemAction"]["actions"][0]:
                     continue
                 if 'liveChatTextMessageRenderer' not in samp["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]:
@@ -57,24 +52,20 @@ try:
                 str1 = str1.replace('[','').replace('{\'text\': \'','').replace('\'}','').replace(', ','').replace(']','')
                 litle_cdata = []
                 litle_cdata.append(str(samp["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["timestampText"]["simpleText"]))
-                print(str(samp["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["timestampText"]["simpleText"]))
+                #print(str(samp["replayChatItemAction"]["actions"][0]["addChatItemAction"]["item"]["liveChatTextMessageRenderer"]["timestampText"]["simpleText"]))
                 litle_cdata.append(str1)
                 comment_data.append(litle_cdata)
+                count+=1
 
-        # next_urlが入手できなくなったら終わり
         except Exception:
             break
 except KeyboardInterrupt:
     sys.exit()
     
-
-# （動画ID）.txt にコメントデータを書き込む
-#url = urlparse(target_url)
-#query = parse_qs(url.query)
-#url = target_url.replace("https://youtu.be/","")
-#title = query["v"][0] + ".txt"
 title = vid + ".csv"
 
-with open(title, mode='w', encoding='CP932', errors='replace', newline='') as f:
+with open("data/"+title, mode='w', encoding='CP932', errors='replace', newline='') as f:
     writer = csv.writer(f)
     writer.writerows(comment_data)
+
+print("finish\n"+str(count)+"comment")
